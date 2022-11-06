f3971494732ab8d5e2ffeaf8265155fe29aaff96
Leonid Alekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2022-11-06 15:39:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,6 @@ yarn config set workspaces-experimental true
 yarn install
 
 docker network create dcm4chee_network
-cd ~/neuro-cabinet-vewers/platform/viewer/.recipes/Nginx-Dcm4che
-docker compose down
-docker compose up -d
+cd ~/neuro-cabinet-vewers/platform/viewer/.recipes/Nginx-Dcm4che && docker compose up -d
 
-cd ~/neuro-cabinet-vewers/platform/viewer/public
-yarn run dev:viewer
+screen -S test bash -c "cd ~/neuro-cabinet-vewers/platform/viewer/public && yarn run dev:viewer"
