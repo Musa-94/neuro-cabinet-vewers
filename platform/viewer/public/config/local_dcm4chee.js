af992653d888b17f62c4912db0cb93ea69eb3f9b
Leonid Alekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2022-11-06 15:13:13 +0300

    Update local_dcm4chee.js

--- platform/viewer/public/config/local_dcm4chee.js
+++ platform/viewer/public/config/local_dcm4chee.js
@@ -1,15 +1,19 @@
 window.config = {
-  // default: '/'
   routerBasename: '/',
-  // default: ''
   showStudyList: true,
-  servers: {
-    dicomWeb: [
-      {
+  extensions: [],
+  modes: [],
+  dataSources: [
+    {
+      friendlyName: 'DCM4CHEE Server',
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
         name: 'DCM4CHEE',
-        wadoUriRoot: 'http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/wado',
-        qidoRoot: 'http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
-        wadoRoot: 'http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoUriRoot:
+          'http://178.170.242.122:8080/dcm4chee-arc/aets/DCM4CHEE/wado',
+        qidoRoot: 'http://178.170.242.122:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoRoot: 'http://178.170.242.122:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
         qidoSupportsIncludeField: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
@@ -17,7 +21,8 @@ window.config = {
           auth: 'admin:admin',
         },
       },
-    ],
-  },
+    },
+  ],
   studyListFunctionsEnabled: true,
+  defaultDataSourceName: 'dicomweb',
 };
